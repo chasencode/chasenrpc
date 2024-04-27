5caf2fc7c127d851572289f94fbc9292a5c55506
chasencode <chasencode@163.com>
2024-04-27 19:40:40 +0800

    fix 提取请求地址

--- rpc-core/src/main/java/cn/chasen/rpc/core/registry/ck/ChasenRegistryCenter.java
+++ rpc-core/src/main/java/cn/chasen/rpc/core/registry/ck/ChasenRegistryCenter.java
@@ -32,6 +32,12 @@ public class ChasenRegistryCenter implements RegistryCenter {
     @Value("${ck.servers}")
     private String servers;
 
+    private static final String REG_PATH = "/reg";
+    private static final String UNREG_PATH = "/unreg";
+    private static final String FIND_ALL_PATH = "/findAll";
+    private static final String VERSION_PATH = "/version";
+
+    private static final String RENWS_PATH = "/renews";
 
     Map<String, Long> VERSIONS = new HashMap<>();
     ScheduledExecutorService consumerExecutor;
@@ -45,9 +51,8 @@ public class ChasenRegistryCenter implements RegistryCenter {
         providerExecutor = Executors.newScheduledThreadPool(1);
         providerExecutor.scheduleAtFixedRate(() -> {
             renews.keySet().forEach(instanceMeta -> {
-                String services = String.join( ",", renews.get(instanceMeta).stream().map(ServiceMeta::toPath).toList());
-                Long timeStamp = HttpInvoker.httpPost(JSON.toJSONString(instanceMeta), servers + "/renews?services=" + services, Long.class);
-                log.debug("====>>> [CkRegistry] : renews instance {} for services {} with timestamp {}", instanceMeta, services, timeStamp);
+                Long timeStamp = HttpInvoker.httpPost(JSON.toJSONString(instanceMeta), renewsPath(renews.get(instanceMeta)), Long.class);
+                log.debug("====>>> [CkRegistry] : renews instance {}  with timestamp {}", instanceMeta, timeStamp);
             });
         }, 5, 5, TimeUnit.SECONDS);
     }
@@ -62,24 +67,26 @@ public class ChasenRegistryCenter implements RegistryCenter {
     @Override
     public void register(ServiceMeta service, InstanceMeta instance) {
         log.info("====>>> [CkRegistry] : register instance {} for {}", instance, service);
-        HttpInvoker.httpPost(JSON.toJSONString(instance), servers + "/reg?service=" + service.toPath(), InstanceMeta.class);
+        HttpInvoker.httpPost(JSON.toJSONString(instance), regPath(service), InstanceMeta.class);
         log.info("====>>> [CkRegistry] : registered  {}", instance);
         renews.add(instance, service);
 
     }
 
+
     @Override
     public void unregister(ServiceMeta service, InstanceMeta instance) {
         log.info("====>>> [CkRegistry] : unregister instance {} for {}", instance, service);
-        HttpInvoker.httpPost(JSON.toJSONString(instance), servers + "/unreg?service=" + service.toPath(), InstanceMeta.class);
+        HttpInvoker.httpPost(JSON.toJSONString(instance), unregPath(service), InstanceMeta.class);
         log.info("====>>> [CkRegistry] : unregistered  {}", instance);
         renews.remove(instance, service);
     }
 
+
     @Override
     public List<InstanceMeta> fetchAll(ServiceMeta service) {
         log.info("====>>> [CkRegistry] : find all instance for {}", service);
-        List<InstanceMeta> instanceMetas = HttpInvoker.httpGet(servers + "/findAll?service=" + service.toPath(), new TypeReference<List<InstanceMeta>>() {
+        List<InstanceMeta> instanceMetas = HttpInvoker.httpGet(findAllPath(service), new TypeReference<List<InstanceMeta>>() {
         });
         log.info("====>>> [CkRegistry] : find all =  {}", instanceMetas);
         return instanceMetas;
@@ -95,7 +102,7 @@ public class ChasenRegistryCenter implements RegistryCenter {
                     return;
                 }
                 long version = VERSIONS.getOrDefault(service.toPath(), -1L);
-                Long newVersion = HttpInvoker.httpGet(servers + "/version?service=" + service.toPath(), Long.class);
+                Long newVersion = HttpInvoker.httpGet(versionPath(service), Long.class);
                 log.info("====>>> [CkRegistry] :  version {}, newVersion {}", version, newVersion);
                 if (newVersion > version) {
                     List<InstanceMeta> instanceMetaList = fetchAll(service);
@@ -108,6 +115,35 @@ public class ChasenRegistryCenter implements RegistryCenter {
         }, 1000, 5000, TimeUnit.MILLISECONDS);
     }
 
+    private String versionPath(ServiceMeta service) {
+        return path(VERSION_PATH, service);
+    }
+
+    private String findAllPath(ServiceMeta service) {
+        return path(FIND_ALL_PATH, service);
+    }
+
+    private String unregPath(ServiceMeta service) {
+        return path(UNREG_PATH, service);
+    }
+
+    private String regPath(ServiceMeta service) {
+        return path(REG_PATH, service);
+    }
+
+    private String renewsPath(List<ServiceMeta> serviceMetaList) {
+        String services = String.join(",", serviceMetaList.stream().map(ServiceMeta::toPath).toList());
+        return path(RENWS_PATH, serviceMetaList);
+    }
+
+    private String path(String context, List<ServiceMeta> serviceMetaList) {
+        String services = String.join(",", serviceMetaList.stream().map(ServiceMeta::toPath).toList());
+        return servers + context + "?services=" + services;
+    }
+
+    private String path(String context, ServiceMeta service) {
+        return servers + context + "?service=" + service;
+    }
 
     private void gracefulShutdown(ScheduledExecutorService executor) {
         executor.shutdown();
